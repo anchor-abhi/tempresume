437aabd64ae27c334a9d059c9d7d105f48aa80a5
anchor-abhi <theabhishek1802@gmail.com>
2022-05-10 00:59:48 +0530

    added loader

--- src/components/downloadResume.js
+++ src/components/downloadResume.js
@@ -3,13 +3,11 @@ import Button from "./Button";
 import jspdf from "jspdf";
 // import "../font/Proxima Nova Font.otf"
 import { TokenContext } from "./context/context";
-import { useContext, useState } from "react";
-
+import { useContext,useEffect, useState } from "react";
 
 
 const DownloadResume = () => {
-
-  const {token} = useContext(TokenContext);
+  const { token } = useContext(TokenContext);
   console.log("token = ", token);
   const handleChange = () => {
     var doc = new jspdf("p", "pt", "a4");
@@ -17,19 +15,21 @@ const DownloadResume = () => {
     doc.addFileToVFS("MyFont.otf", myFont);
     doc.addFont("MyFont.otf", "MyFont", "normal");
     doc.setFont("MyFont");
-    
+
     doc.html(document.querySelector("#mainDiv"), {
       callback: function (pdf) {
         pdf.save("Resume.pdf");
       },
     });
   };
+    
   return (
     <>
       <Button click={handleChange} />
       <Resume />
     </>
   );
+  
 };
 
 export default DownloadResume;
