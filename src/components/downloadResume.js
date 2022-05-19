4baeba66a3cf0336b938c0023854e7c69299465c
anchor-abhi <theabhishek1802@gmail.com>
2022-05-19 14:12:53 +0530

    cleaning code

--- src/components/downloadResume.js
+++ src/components/downloadResume.js
@@ -1,13 +1,8 @@
 import Resume from "./resume";
 import Button from "./Button";
 import jspdf from "jspdf";
-import { TokenContext } from "./context/context";
-import { useContext,useEffect, useState } from "react";
-
 
 const DownloadResume = () => {
-  const { token } = useContext(TokenContext);
-  console.log("token = ", token);
   const handleChange = () => {
     var doc = new jspdf("p", "pt", "a4");
     doc.html(document.querySelector("#mainDiv"), {
