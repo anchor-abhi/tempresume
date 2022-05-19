58b90580ac563ff19f41197de321cbadbacb37ed
isharaman8 <isharaman8@gmail.com>
2022-05-19 13:30:04 +0530

    Created basic resume template

--- src/Pages/TemplateSelector.jsx
+++ src/Pages/TemplateSelector.jsx
@@ -12,6 +12,7 @@ import MyButton from "../components/Button";
 import jspdf from "jspdf";
 
 import "./templatess.css";
+import Resume from "../components/resume";
 
 const imageArr = [
 	{ img: "downloadresume", component: 1 },
@@ -22,16 +23,16 @@ const imageArr = [
 ];
 
 const handleChange = () => {
-    var doc = new jspdf("p", "pt", "a4");
-	
-    doc.html(document.querySelector("#mainDiv"), {
-      callback: function (pdf) {
-		var pageCount = doc.internal.getNumberOfPages();
-		doc.deletePage(pageCount);
-        pdf.save("Resume.pdf");
-      },
-    });
-  };
+	var doc = new jspdf("p", "pt", "a4");
+
+	doc.html(document.querySelector("#mainDiv"), {
+		callback: function (pdf) {
+			var pageCount = doc.internal.getNumberOfPages();
+			doc.deletePage(pageCount);
+			pdf.save("Resume.pdf");
+		},
+	});
+};
 
 function TemplateSelector() {
 	const [open, setOpen] = useState(false);
@@ -44,60 +45,60 @@ function TemplateSelector() {
 
 	return (
 		<>
-		<MyButton click={handleChange}/>
-		<Box style={{ marginTop: 0 }}>
-			{currentTemplate === 1 ? (
-				<DownloadResume />
-			) : currentTemplate === 2 ? (
-				<Temp2 /> 
-			) : currentTemplate === 3 ? (
-				<Template />
-			) : currentTemplate === 4 ? (
-				<Temp4 />
-			) : currentTemplate === 5 ? (
-				<Temp5 />
-			) : (
-				<></>
-			)}
+			<MyButton click={handleChange} />
+			<Box style={{ marginTop: 0 }}>
+				{currentTemplate === 1 ? (
+					<Resume />
+				) : currentTemplate === 2 ? (
+					<Temp2 />
+				) : currentTemplate === 3 ? (
+					<Template />
+				) : currentTemplate === 4 ? (
+					<Temp4 />
+				) : currentTemplate === 5 ? (
+					<Temp5 />
+				) : (
+					<></>
+				)}
 
-			<Button
-				variant="outlined"
-				onClick={() => setOpen(true)}
-				style={{ position: "fixed", bottom: 10, left: 10 }}
-			>
-				See Templates
-			</Button>
-			<Drawer
-				BackdropProps={{ invisible: true }}
-				open={open}
-				PaperProps={{
-					sx: {
-						width: "350px",
-						display: "flex",
-						flexDirection: "column",
-						justifyContent: "flex-start",
-						alignItems: "center",
-						gap: "20px",
-					},
-				}}
-				onClose={() => setOpen(false)}
-			>
-				{imageArr.map((oneimg) => {
-					return (
-						<img
-							key={oneimg.img}
-							className="templateImages"
-							style={{ cursor: "pointer", margin: "10px 0" }}
-							src={`/images/templatess/${oneimg.img}.png`}
-							onClick={() => setCurrentTemplate(oneimg.component)}
-							alt="template image"
-							width="50%"
-							height="auto"
-						/>
-					);
-				})}
-			</Drawer>
-		</Box>
+				<Button
+					variant="outlined"
+					onClick={() => setOpen(true)}
+					style={{ position: "fixed", bottom: 10, left: 10 }}
+				>
+					See Templates
+				</Button>
+				<Drawer
+					BackdropProps={{ invisible: true }}
+					open={open}
+					PaperProps={{
+						sx: {
+							width: "350px",
+							display: "flex",
+							flexDirection: "column",
+							justifyContent: "flex-start",
+							alignItems: "center",
+							gap: "20px",
+						},
+					}}
+					onClose={() => setOpen(false)}
+				>
+					{imageArr.map((oneimg) => {
+						return (
+							<img
+								key={oneimg.img}
+								className="templateImages"
+								style={{ cursor: "pointer", margin: "10px 0" }}
+								src={`/images/templatess/${oneimg.img}.png`}
+								onClick={() => setCurrentTemplate(oneimg.component)}
+								alt="template image"
+								width="50%"
+								height="auto"
+							/>
+						);
+					})}
+				</Drawer>
+			</Box>
 		</>
 	);
 }
--- src/components/downloadResume.js
+++ src/components/downloadResume.js
@@ -6,26 +6,26 @@ import { useContext,useEffect, useState } from "react";
 
 
 const DownloadResume = () => {
-  const { token } = useContext(TokenContext);
-  console.log("token = ", token);
-  const handleChange = () => {
-    var doc = new jspdf("p", "pt", "a4");
-    doc.html(document.querySelector("#mainDiv"), {
-      callback: function (pdf) {
-        var pageCount = doc.internal.getNumberOfPages();
-		    doc.deletePage(pageCount);
-        pdf.save("Resume.pdf");
-      },
-    });
-  };
-    
-  return (
-    <>
-      <Button click={handleChange} />
-      <Resume />
-    </>
-  );
-  
+   const { token } = useContext(TokenContext);
+   console.log("token = ", token);
+   const handleChange = () => {
+      var doc = new jspdf("p", "pt", "a4");
+      doc.html(document.querySelector("#mainDiv"), {
+         callback: function (pdf) {
+            var pageCount = doc.internal.getNumberOfPages();
+            doc.deletePage(pageCount);
+            pdf.save("Resume.pdf");
+         },
+      });
+   };
+
+   return (
+      <>
+         {/*<Button click={handleChange} /> */}
+         <Resume />
+      </>
+   );
+
 };
 
 export default DownloadResume;
