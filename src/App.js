21ff8ecba28595d9469d2fa1959d791127fc086c
Aman Kumar <amankumar@pop-os.localdomain>
2022-06-03 16:10:17 +0530

    added wrong links functionalities

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import ResponsiveAppBar from "./components/navbar";
 import AddRoute from "./components/routes";
 
 function App() {
-   return (
-      <div style={{width:"100%",
-         display:"flex",
-         justifyContent:"center", 
-         alignItems:"center",
-         flexDirection:"column"
-      }} className="App">
-
-         <ResponsiveAppBar />
-         <AddRoute/>
-      </div>
-   );
+	return (
+		<div
+			style={{
+				width: "100%",
+				display: "flex",
+				justifyContent: "center",
+				alignItems: "center",
+				flexDirection: "column",
+			}}
+			className="App"
+		>
+			<ResponsiveAppBar />
+			<AddRoute />
+		</div>
+	);
 }
 
 export default App;
