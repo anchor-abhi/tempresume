310bb274104b332e8a7934c9d5914e0f6f036b00
anchor-abhi <theabhishek1802@gmail.com>
2022-06-10 13:12:48 +0530

    temp6 check

--- src/components/routes.js
+++ src/components/routes.js
@@ -7,22 +7,24 @@ import TemplateSelector from "../Pages/TemplateSelector";
 import { Demo } from "./Form/Demo";
 import UpdatePass from "./updatePass";
 import { Template6 } from "./newTemplates/template6/Template";
+import ResponsiveAppBar from "./navbar";
 import {Download}  from "../Pages/download";
 
 export default function AddRoute() {
-	return (
-		<>
-			<Routes>
-				<Route path="/" element={<UserAuth />} />
-				<Route path="/signin" element={<SignIn />} />
-				<Route path="/signup" element={<SignUp />} />
-				<Route path="/createform" element={<Form />} />
-				<Route path="/image" element={<Demo />} />
+  return (
+    <>
+	<ResponsiveAppBar/>
+      <Routes>
+        <Route path="/" element={<UserAuth />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/createform" element={<Form />} />
+        <Route path="/image" element={<Demo />} />
         <Route path="/temp6" element={<Template6 />} />
-				<Route path="/selecttemplate" element={<TemplateSelector />} />
-				<Route path="/forgot-password/:token" element={<UpdatePass />} />
+        <Route path="/selecttemplate" element={<TemplateSelector />} />
+        <Route path="/forgot-password/:token" element={<UpdatePass />} />
         <Route path="/download" element={<Download />} />
-			</Routes>
-		</>
-	);
+      </Routes>
+    </>
+  );
 }
